5e66b16654967d454fe58261d6b35b8f6f61da13
Davide Pescatori <pescatoridavide7@gmail.com>
2023-05-04 20:14:45 +0200

    date validation e progetto terminato

--- app/Http/Requests/GameRequest.php
+++ app/Http/Requests/GameRequest.php
@@ -17,12 +17,27 @@ class GameRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|min:3|max:20',
+            'producer' => 'required|max:100',
+            'cover' => 'required|image',
+            'description' => 'required',
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'Inserisci il titolo del videogame',
+            'title.min' => 'Il titolo deve essere minimo di 3 caratteri',
+            'title.max' => 'Il titolo deve essere massimo di 50 caratteri',
+            'cover.image' => 'La copertina deve essere una foto',
+            'description.required' => 'Inserisci la descrizione del gioco',
+        ];
+
     }
 }
--- resources/views/game/create.blade.php
+++ resources/views/game/create.blade.php
@@ -3,25 +3,36 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-8">
                 <form action="{{ route('game.store') }}" method="POST" class="p-5 shadow" enctype="multipart/form-data">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     @csrf
                     <div class="mb-3">
-                        <label for="title" class="form-label">Titolo videogame</label>
-                        <input type="text" name="title" class="form-control" id="title">
+                        <label for="title" class="form-label">Titolo videogame <span class="text-danger small">*</span></label>
+                        <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
                     </div>
 
                     <div class="mb-3">
-                        <label for="producer" class="form-label">Produttore</label>
-                        <input type="text" name="producer" class="form-control" id="producer">
+                        <label for="producer" class="form-label">Produttore <span class="text-danger small">*</span></label>
+                        <input type="text" name="producer" class="form-control" id="producer" value="{{ old('producer') }}">
                     </div>
 
                     <div class="mb-3">
-                        <label for="cover" class="form-label">Immagine di copertina</label>
+                        <label for="cover" class="form-label">Immagine di copertina <span class="text-danger small">*</span></label>
                         <input type="file" name="cover" class="form-control" id="cover">
                     </div>
 
                     <div class="mb-3">
-                        <label for="description" class="form-label">Breve descrizione</label>
-                        <textarea name="description" id="description" cols="30" rows="7" class="form-control"></textarea>
+                        <label for="description" class="form-label">Breve descrizione <span class="text-danger small">*</span></label>
+                        <textarea name="description" id="description" cols="30" rows="7" class="form-control">{{ old('description') }}</textarea>
                     </div>
                   
                     <button type="submit" class="btn btn-dark">Inserisci il tuo videogioco</button>
