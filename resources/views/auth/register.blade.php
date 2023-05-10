3babb047e4f5be249a1fc49e29d614e6108ddd73
Davide Pescatori <pescatoridavide7@gmail.com>
2023-05-10 16:33:07 +0200

    impostate register-login-logout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,9 @@
-<x-layout header="Accedi">
+<x-layout header="Register">
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-12 col-md-8">
 
-                <form class="p-5 shadow" method="POST" action="{{ route('login') }}">
+                <form class="p-5 shadow" method="POST" action="{{ route('register') }}">
                         @if ($errors->any())
                             <div class="alert alert-danger">
                                 <ul>
@@ -19,15 +19,24 @@
                         <label for="email" class="form-label">Indirizzo email</label>
                         <input type="email" name="email" class="form-control" id="email">
                     </div>
+
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nome utente</label>
+                        <input type="text" name="name" class="form-control" id="name">
+                    </div>
+
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="password">
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" name="remember" class="form-check-input" id="remember">
-                        <label class="form-check-label" for="remember">Ricordami</label>
+
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Conferma password</label>
+                        <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
                     </div>
-                    <button type="submit" class="btn btn-danger">Accedi</button>
+
+                   
+                    <button type="submit" class="btn btn-danger">Registrati</button>
                 </form>
 
             </div>
